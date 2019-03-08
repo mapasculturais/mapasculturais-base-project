6a03733f578d5885ee25bb36219ae54c984ca470
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2019-03-08 17:56:32 -0300

    melhorias no arquivo de configuração para desenvolvimento

--- compose/local/config.d/dev.php
+++ compose/local/config.d/dev.php
@@ -4,13 +4,13 @@ return [
     'doctrine.isDev' => false,
 
 
-    // LOG
+    // LOG --------------------
     'slim.log.level'        => \Slim\Log::DEBUG,
     'slim.log.enabled'      => true,
 
     // app.log.hook aceita regex para filtrar quais hooks são exibidos no output, 
     // ex: "panel", "^template", "template\(site\.index\.*\):before"
-    'app.log.hook'          => true, 
+    'app.log.hook'          => false, 
     // 'app.log.query'         => true,
     // 'app.log.requestData'   => true,
     // 'app.log.translations'  => true,
@@ -19,9 +19,19 @@ return [
     // 'app.log.assets'        => true,
 
 
+    // MAILER -----------------
+    // 'mailer.user'       => 'you@gmail.com', 
+    // 'mailer.psw'        => 'passwd', 
+    // 'mailer.protocol'   => 'SSL', 
+    // 'mailer.server'     => 'smtp.gmail.com', 
+    // 'mailer.port'       => '465', 
+    // 'mailer.from'       => 'you@gmail.com', 
+    // 'mailer.alwaysTo'   => 'you@gmail.com', // todos os emails serão enviados para este endereço
 
-    // AUTH
-    'auth.provider' => $fake ? 'Fake' : '\MultipleLocalAuth\Provider',
+
+    // AUTH -------------------
+    // 'auth.provider' => 'Fake', 
+    'auth.provider' => '\MultipleLocalAuth\Provider',
     'auth.config' => array(
         'salt' => env('AUTH_SALT', null),
         'timeout' => '24 hours',
