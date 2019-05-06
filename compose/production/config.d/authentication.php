90fb62e19df49e52141ff1f16d398c4d0f242d1d
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2019-05-06 18:08:26 -0300

    usa HTTP_HOST como base do url na configuração da autenticação de produção

--- compose/production/config.d/authentication.php
+++ compose/production/config.d/authentication.php
@@ -1,4 +1,11 @@
 <?php
+// creating base url
+$prot_part = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] ? 'https://' : 'http://';
+//added @ for HTTP_HOST undefined in Tests
+$host_part = @$_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']);
+if(substr($host_part,-1) !== '/') $host_part .= '/';
+$_APP_BASE_URL = $prot_part . $host_part;
+
 return [
     'auth.provider' => '\MultipleLocalAuth\Provider',
     'auth.config' => array(
@@ -14,13 +21,13 @@ return [
             'LinkedIn' => array(
                 'api_key' => env('AUTH_LINKEDIN_API_KEY', null),
                 'secret_key' => env('AUTH_LINKEDIN_SECRET_KEY', null),
-                'redirect_uri' => APP_BASE_URL . 'autenticacao/linkedin/oauth2callback',
+                'redirect_uri' => $_APP_BASE_URL . 'autenticacao/linkedin/oauth2callback',
                 'scope' => env('AUTH_LINKEDIN_SCOPE', 'r_emailaddress')
             ),
             'Google' => array(
                 'client_id' => env('AUTH_GOOGLE_CLIENT_ID', null),
                 'client_secret' => env('AUTH_GOOGLE_CLIENT_SECRET', null),
-                'redirect_uri' => APP_BASE_URL . 'autenticacao/google/oauth2callback',
+                'redirect_uri' => $_APP_BASE_URL . 'autenticacao/google/oauth2callback',
                 'scope' => env('AUTH_GOOGLE_SCOPE', 'email'),
             ),
             'Twitter' => array(
