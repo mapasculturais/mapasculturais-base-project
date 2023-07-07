54d8a26dd0897b1edbb7e96d8416508934c4e3d7
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-07-07 00:49:24 -0300

    adequação dos arquivos de configuração para o mapas v7

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -2,10 +2,6 @@
 
 return [
     'plugins' => [
-        'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical', 'config' => ['step' => 0.5]],
-        'EvaluationMethodSimple' => ['namespace' => 'EvaluationMethodSimple'],
-        'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
-        
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
         'SamplePlugin' => ['namespace' => 'SamplePlugin'],
     ]
