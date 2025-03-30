e6c0faaa43eb8692e171d3277251c75d0010ac38
Victor Ferreira <vitfera@hotmail.com>
2025-03-30 02:20:49 -0300

    install plugin Accessibility

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -4,5 +4,6 @@ return [
     'plugins' => [
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
         'SamplePlugin' => ['namespace' => 'SamplePlugin'],
+        'Accessibility' => ['namespace' => 'Accessibility'],
     ]
 ];
