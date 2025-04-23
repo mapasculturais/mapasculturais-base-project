56f8d8d8b2bac44ebee1bef41ef737af9854552f
LFelipeLima <30807726+LFelipeLima@users.noreply.github.com>
2025-04-23 13:33:59 -0300

    Update 0.main.php

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -5,10 +5,8 @@ return [
     'app.siteDescription' => 'O Mapa Cultural é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e editais',
     
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
-    'themes.active' => 'BaseV2',
-    'themes.active' => 'Subsite',
-    //'themes.active' => 'MapasCulturais\Themes\BaseV2',
-    //'themes.active' => 'MapasCulturais\Themes\Subsite',
+    'themes.active' => 'MapasCulturais\Themes\BaseV2',
+    'themes.active' => 'MapasCulturais\Themes\Subsite',
 
     // Ids dos selos verificadores. Para utilizar múltiplos selos informe os ids separados por vírgula.
     'app.verifiedSealsIds' => '1', 
