a739dfbb4e5f78648a532544f9cf84a9c520ecb0
LFelipeLima <30807726+LFelipeLima@users.noreply.github.com>
2025-04-23 11:16:49 -0300

    Ajustes no main

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -1,11 +1,13 @@
 <?php 
 
 return [
-    'app.siteName' => 'Mapas Culturais Base Project',
-    'app.siteDescription' => 'O Mapas Culturais é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
+    'app.siteName' => 'Mapa Cultural de Aracaju',
+    'app.siteDescription' => 'O Mapa Cultural é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e editais',
     
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
-    'themes.active' => 'MapasCulturais\Themes\BaseV2',
+    'themes.active' => 'BaseV2',
+    'themes.active' => 'Subsite',
+    //'themes.active' => 'MapasCulturais\Themes\BaseV2',
 
     // Ids dos selos verificadores. Para utilizar múltiplos selos informe os ids separados por vírgula.
     'app.verifiedSealsIds' => '1', 
