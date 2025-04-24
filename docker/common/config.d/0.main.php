5bb6ce1291e382288764a30993b77d8adc1fc18a
LFelipeLima <30807726+LFelipeLima@users.noreply.github.com>
2025-04-23 23:38:16 -0300

    Update 0.main.php

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -6,7 +6,7 @@ return [
     
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
     'themes.active' => 'MapasCulturais\Themes\BaseV2',
-    'themes.active' => 'MapasCulturais\Themes\Subsite',
+    //'themes.active' => 'MapasCulturais\Themes\Subsite',
 
     // Ids dos selos verificadores. Para utilizar múltiplos selos informe os ids separados por vírgula.
     'app.verifiedSealsIds' => '1', 
